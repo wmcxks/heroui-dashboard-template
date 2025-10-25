3dd5bef4844c5bd8a3b0cd32a093f0e45a64dd61
wmcxks <wmcxks@mail.com>
2025-10-25 15:18:35 +0900

    fix ThemeProviderProps link

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,7 +2,7 @@
 import * as React from "react";
 import { HeroUIProvider } from "@heroui/system";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ThemeProviderProps } from "next-themes/dist/types";
+import type { ThemeProviderProps } from "next-themes";
 
 export interface ProvidersProps {
   children: React.ReactNode;
